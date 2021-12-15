0edc3f16dca763fb265bf8582dc88864a06d1eb9
HynekS <hyneksvacha@seznam.cz>
2021-12-15 08:14:17 +0100

    Add type to BreadcrumbsWrapper props

--- components/breadcrumbs-wrapper.tsx
+++ components/breadcrumbs-wrapper.tsx
@@ -1,5 +1,12 @@
+import type { ReactNode } from "react";
+
+type BreadcrumbsWrapperProps = {
+  children?: ReactNode;
+};
 // Min height is set to prevent layout jumps after breadcrumbs loads on client
-export default function BreadcrumbsWrapper({ children }) {
+export default function BreadcrumbsWrapper({
+  children,
+}: BreadcrumbsWrapperProps) {
   return (
     <div tw="border-t border-b border-gray-300 height[2.75rem]">
       <div tw="mx-auto px-6 max-width[60em] h-full">{children}</div>
